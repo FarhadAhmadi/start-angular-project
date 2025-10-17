683392202f6f4261e6519cfe7456272a7dd1a17e
FarhadAhmadi <farhadahmadi1413@gmail.com>
2025-10-17 17:20:02 +0330

    Init Angular Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent , NgIf , NgFor],
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  selectedUserId!: string;
+  selectedUserName!: string;
+
+  get hasName(): boolean {
+    return !!this.selectedUserName?.trim();
+  }
+
+  OnSelectUser(id: string): void {
+    this.selectedUserId = id;
+    this.selectedUserName = this.users.find((u) => u.id === id)?.name ?? '';
+  }
+
+  changeUserName(): void {
+    console.log('Before change:', this.users);
+    // فقط اسم اولین کاربر رو تغییر می‌دیم
+    this.users[0] = { ...this.users[0], name: 'Changed Name' };
+    console.log('After change:', this.users);
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+interface  User  {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter();
+
+  get imagePath(): string {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+    ngOnInit() {
+    console.log('✅ UserComponent initialized for:', this.user.name);
+  }
+
+  ngOnChanges() {
+    console.log('♻️ UserComponent changed for:', this.user.name);
+  }
+
+  onSelectUser(): void {
+    this.select.emit(this.user.id);
+  }
+}
