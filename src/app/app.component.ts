396f1cb6c8a05ff5a7ca2d62eb5e6b8cd6e9ccf8
FarhadAhmadi <farhadahmadi1413@gmail.com>
2025-10-18 22:47:31 +0330

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
-
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent , NgIf , NgFor],
+  // imports: [HeaderComponent, UserComponent, TasksComponent , NgIf , NgFor],
 })
 export class AppComponent {
   users = DUMMY_USERS;
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,9 @@
 import { Component, inject, Input } from '@angular/core';
-import { TaskComponent } from './task/task.component';
-import { NgFor, NgIf } from '@angular/common';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { NewTaskData, Task } from './task/task.model';
-import { CardComponent } from '../shared/card/card.component';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NgFor, NewTaskComponent, NgIf, CardComponent],
+  standalone: false,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
